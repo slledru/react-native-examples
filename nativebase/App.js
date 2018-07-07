403739a769146ff0918c27bd0a61908ebf6886a3
slledru <slledru@msn.com>
2018-07-07 15:35:58 -0600

    added padding

--- nativebase/App.js
+++ nativebase/App.js
@@ -8,14 +8,14 @@ import {
 export default class App extends Component {
   render() {
     return (
-      <Container>
+      <Container style={{ paddingTop: 20 }}>
         <ImageBackground
           source={{ uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530998057/App%20Icons/1.a_base-02.png' }}
           style={{ width: '100%', height: '100%' }}>
-          <Header  style={{ height: 70, backgroundColor: 'transparent' }}>
+          <Header  style={{ height: 55, backgroundColor: 'transparent' }}>
             <Left/>
             <Body >
-              <Title style={{ marginTop: 25 }}>
+              <Title style={{ marginTop: 15 }}>
                 <Thumbnail square style={{ borderWidth: 1, borderColor: '#FFF' }}
                   source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530995250/App%20Icons/1024x1024_opt3.png'}} />
               </Title>
