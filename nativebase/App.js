0bfa7016936a02186f5daba32f66b7a8f7023eb1
slledru <slledru@msn.com>
2018-07-07 15:31:11 -0600

    set background image

--- nativebase/App.js
+++ nativebase/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ImageBackground } from 'react-native'
 import {
   Container, Header, Left, Right,
   Content, Body, Title, Icon,
@@ -6,39 +7,43 @@ import {
 } from 'native-base';
 export default class App extends Component {
   render() {
-    const uri = "https://facebook.github.io/react-native/docs/assets/favicon.png";
     return (
       <Container>
-        <Header>
-          <Left/>
-          <Body>
-            <Title style={{ marginTop: 20 }}>
-              <Thumbnail square
-                source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530995250/App%20Icons/1024x1024_opt3.png'}} />
-            </Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content />
-        <Footer style={{ height: 70 }}>
-          <FooterTab>
-            <Button>
-              <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996064/App%20Icons/Home_Icon.png'}} />
-            </Button>
-            <Button>
-              <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996065/App%20Icons/Add_Event_Icon.png'}} />
-            </Button>
-            <Button>
-              <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996063/App%20Icons/My_Events_Icon.png'}} />
-            </Button>
-            <Button>
-              <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996065/App%20Icons/My_Messages_Icon.png'}} />
-            </Button>
-            <Button>
-              <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996065/App%20Icons/My_Profile_Icon.png'}} />
-            </Button>
-          </FooterTab>
-        </Footer>
+        <ImageBackground
+          source={{ uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530998057/App%20Icons/1.a_base-02.png' }}
+          style={{ width: '100%', height: '100%' }}>
+          <Header  style={{ height: 70, backgroundColor: 'transparent' }}>
+            <Left/>
+            <Body >
+              <Title style={{ marginTop: 25 }}>
+                <Thumbnail square style={{ borderWidth: 1, borderColor: '#FFF' }}
+                  source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530995250/App%20Icons/1024x1024_opt3.png'}} />
+              </Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content>
+          </Content>
+          <Footer style={{ height: 70, backgroundColor: 'transparent' }}>
+            <FooterTab>
+              <Button>
+                <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996064/App%20Icons/Home_Icon.png'}} />
+              </Button>
+              <Button>
+                <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996065/App%20Icons/Add_Event_Icon.png'}} />
+              </Button>
+              <Button>
+                <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996063/App%20Icons/My_Events_Icon.png'}} />
+              </Button>
+              <Button>
+                <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996065/App%20Icons/My_Messages_Icon.png'}} />
+              </Button>
+              <Button>
+                <Thumbnail round source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530996065/App%20Icons/My_Profile_Icon.png'}} />
+              </Button>
+            </FooterTab>
+          </Footer>
+        </ImageBackground>
       </Container>
     );
   }
