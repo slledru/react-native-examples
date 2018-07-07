97eb627cb7645c8108c4663a64771e5d4b7bef19
slledru <slledru@msn.com>
2018-07-07 17:09:50 -0600

    background image is packaged

--- nativebase/App.js
+++ nativebase/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import { ImageBackground } from 'react-native'
+import { ImageBackground, Dimensions } from 'react-native'
 import {
   Container, Header, Left, Right,
   Content, Body, Title, Icon,
   Footer, FooterTab, Button, Thumbnail
 } from 'native-base';
+
+const { width } = Dimensions.get('window')
+
+const backgroundImage = require('./assets/1_a_base_02.png')
+
 export default class App extends Component {
   render() {
     return (
       <Container style={{ paddingTop: 20 }}>
         <ImageBackground
-          source={{ uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530998057/App%20Icons/1.a_base-02.png' }}
+          source={ backgroundImage }
           style={{ width: '100%', height: '100%' }}>
-          <Header  style={{ height: 55, backgroundColor: 'transparent' }}>
+          <Header style={{ height: 62, width: width, backgroundColor: 'transparent', marginTop: 0, marginBottom: 1, paddingLeft: 0, paddingRight: 0, borderBottomColor: '#FFF', borderBottomWidth: 1 }}>
             <Left/>
             <Body >
               <Title style={{ marginTop: 15 }}>
-                <Thumbnail square style={{ borderWidth: 1, borderColor: '#FFF' }}
+                <Thumbnail square style={{ width: 60, height: 60, borderWidth: 1, borderRadius: 5, borderColor: '#FFF' }}
                   source={{uri: 'https://res.cloudinary.com/drf6ttx34/image/upload/v1530995250/App%20Icons/1024x1024_opt3.png'}} />
               </Title>
             </Body>
             <Right />
           </Header>
-          <Content>
+          <Content style={{ borderColor: '#FFF', borderBottomWidth: 1 }}>
           </Content>
           <Footer style={{ height: 70, backgroundColor: 'transparent' }}>
             <FooterTab>
